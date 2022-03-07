5a3f81cfefedbda4826a013fdd8874af9ca4a595
pajith551@gmail.com <pajith551@gmail.com>
2022-03-07 21:54:07 +0530

    express

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const req = require("express/lib/request");
 
 const app = express();
 
 app.get("/", function(req, res){
     res.send("<h1>Hello✨</h1>");
+});
+
+app.get("/contact", function(){
+    req.send("contact me");
 });
 
 app.listen(3000, function(){

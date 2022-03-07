0774afa36cb5cf5a449c3d61fa12fcf783f026c3
pajith551@gmail.com <pajith551@gmail.com>
2022-03-07 22:41:24 +0530

    nodemon

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ app.get("/", function(req, res){
     res.send("<h1>Hello✨</h1>");
 });
 
-app.get("/contact", function(){
-    req.send("contact me");
+app.get("/contact", function(req, res){
+    res.send("contact me");
+});
+
+app.get("/about", function(req, res){
+    res.send("My name is Ajith P");
 });
 
 app.listen(3000, function(){
